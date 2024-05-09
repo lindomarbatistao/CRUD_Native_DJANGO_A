e0eb23bc9c86812c8988722b1844dc5bbbe8cbb4
Lindomar <lindomarbatistao@gmail.com>
2024-05-09 09:43:12 -0300

    09-05-2024 v1

--- back/api/models.py
+++ back/api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 import uuid
 
 def upload_image(instance, filename):
-    return filename
+    uuid_str = str(uuid.uuid4())
+    return f"{uuid_str}_{filename}"
     
 
 class Cliente(models.Model):
@@ -15,3 +16,8 @@ class Cliente(models.Model):
     cep = models.CharField(max_length=255)
     numero = models.CharField(max_length=255)
     image = models.ImageField(upload_to=upload_image, blank=True, null=True)
+
+
+
+
+    
